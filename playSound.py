3f932d1a267ca5a57123053e9c0236f4a79b1f5b
zthijs <56317280+zthijs@users.noreply.github.com>
2021-11-04 13:19:45 +0100

    .

--- playSound.py
+++ playSound.py
@@ -1,9 +1,8 @@
 import pygame
 
-pygame.mixer.init()
-
 def play():
 
+    pygame.mixer.init()
     pygame.mixer.music.load("assets/Harry_Potter_theme.wav")
     pygame.mixer.music.play(loops = 0)
 
